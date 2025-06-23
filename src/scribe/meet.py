96fcd0815e6a8e8a67a0cc4d75aa6b99b7a31228
Elio Nakhle <elio.nakhle@agile-robots.com>
2025-06-23 13:42:34 +0200

    cleanup if error + transcribe wav before converting to mp3

--- src/scribe/meet.py
+++ src/scribe/meet.py
@@ -24,15 +24,29 @@ def convert():
     os.remove(MEETING_RECORD)
 
 
+def cleanup():
+    """
+    Clean up the meeting directory and its contents.
+    This function is called when the script is interrupted or exits.
+    """
+    if MEETING_DIR.exists():
+        for file in MEETING_DIR.iterdir():
+            file.unlink()
+        MEETING_DIR.rmdir()
+
+
 @app.command()
 def meet():
     """
     Main function to handle the meeting workflow.
     It orchestrates the recording, converting, and transcribing of the meeting audio.
     """
-    record()
-    convert()
-    transcribe(COMPRESSED_MEETING_RECORD, str(MEETING_DIR / MEETING_NAME))
+    try:
+        record()
+        transcribe(MEETING_RECORD, str(MEETING_DIR / MEETING_NAME))
+        convert()
+    except Exception:
+        cleanup()
 
 
 if __name__ == "__main__":
